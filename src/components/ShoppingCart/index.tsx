8ac0b49b6bfec1dd811feafb71ded9f1366fabb4
Adepejustephen <adepejuifeoluwa97@gmail.com>
2022-08-26 10:11:18 +0100

    optimized images

--- src/components/ShoppingCart/index.tsx
+++ src/components/ShoppingCart/index.tsx
@@ -13,21 +13,22 @@ export const ShoppingCart = () => {
       <div className="cart__container">
         <div className="cart__topBar">
           <h4 className="cart__title">Cart</h4>
-          <FaTimes onClick={closeCart}/>
+          <FaTimes className="cart__close--btn" onClick={closeCart} />
         </div>
         <div className="cart__body">
           {cartItems.map((item) => (
             <CartItem key={item.id} {...item} />
           ))}
 
-                  <span className="cart__total">
-                      Total: {formatCurrency(cartItems.reduce((total, cartItem) => {
-                          
-                     const item = items.find(i => i.id === cartItem.id);
-                          return total + (item?.price || 0) * cartItem.quantity
-                      }, 0
-                     ))} 
-                  </span>
+          <span className="cart__total">
+            Total:{" "}
+            {formatCurrency(
+              cartItems.reduce((total, cartItem) => {
+                const item = items.find((i) => i.id === cartItem.id);
+                return total + (item?.price || 0) * cartItem.quantity;
+              }, 0)
+            )}
+          </span>
         </div>
       </div>
     </div>
